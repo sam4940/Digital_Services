6a23c42234b93ede1a3e7e1d2423ff07ca83702e
sam4940 <sammichael494@gmail.com>
2026-04-20 23:58:23 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -58,9 +58,9 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
-  });
+  app.get('/api', (req, res) => {
+  res.send('API is running');
+});
 }
 // start server
 const PORT = process.env.PORT || 5000;
